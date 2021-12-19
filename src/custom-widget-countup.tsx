55da33baf36a67ed047a00c73a6628fcae339839
Max Scholz <max.scholz@maximize-it.eu>
2021-12-19 17:50:19 +0100

    update

--- src/custom-widget-countup.tsx
+++ src/custom-widget-countup.tsx
@@ -18,7 +18,7 @@ export interface CustomWidgetCountupProps extends BlockAttributes {
 
 export const CustomWidgetCountup = ({ title, showtitle, titlecolor, boxescolorbg, boxescolorborder, boxescolortext, countupdate, contentLanguage }: CustomWidgetCountupProps): ReactElement => {
   
-  return <CountupView
+  return <div><CountupView
     {...{
       title,
       showtitle,
@@ -29,6 +29,8 @@ export const CustomWidgetCountup = ({ title, showtitle, titlecolor, boxescolorbg
       countupdate,
       contentLanguage,
     }}
-  />
+  /><br/><br/><div data-widget-conf-grid-type="100" data-widget-type="Section" data-widget-src="internal://staffbase.content.widgets.Section"><div>
+  <div data-widget-conf-type="warning" data-widget-on-card="true" data-widget-type="Infobox" data-widget-src="internal://staffbase.content.widgets.Infobox"><p>PROTOTYPES AND FOR DEMO PURPOSES ONLY!</p><p>NOT FOR PRODUCTION USE!</p><p>NOT AVAILABLE TO CUSTOMERS!</p></div>
+  </div></div></div>
 };
 
